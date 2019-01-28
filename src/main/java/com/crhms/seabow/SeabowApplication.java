59e98ee3f6e7cd6340d868358a17e13ef7df28af
hankuikuide <hankuikuide@163.com>
2019-01-28 11:02:29 +0800

    create proj

---
+++ src/main/java/com/crhms/seabow/SeabowApplication.java
@@ -0,0 +1,14 @@
+package com.crhms.seabow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeabowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeabowApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/crhms/seabow/SeabowApplicationTests.java
@@ -0,0 +1,17 @@
+package com.crhms.seabow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeabowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
